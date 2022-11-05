c8c491f93e74182c6bddbe506625714f481c4868
David <davidmollera@hotmail.com>
2022-11-05 18:11:05 -0500

    added a function to always display 10 stars on the rating, but calculate 10 - score to display empty stars

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
 
 
 
-// async function renderAnime() {
-//     const animeHTML = document.querySelector(".anime__container");
-//     const animes = await getData()
-//     const anime = animes["data"].map((anime) => {
-//         return `<div class="anime__info--container">
-//         <figure class="anime__img--wrapper">
-//         <img src="${anime.images.jpg.large_image_url}" alt="" class="anime__img">
-//         </figure>
-//         <div class= "anime__organizer">
-//         <h3 class="anime__title">${anime.title} ${animeYear(anime.year)}<br> <a href="${anime.url}" class="anime__link" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a></h3>
-//         <div class="anime__rating">
-//         ${animeRating(anime.score)}
-//         </div>
-//         <p class="anime__synopsis">${anime.synopsis}</p>
-//         </div>
-//         </div>`
-//     }).join('')
-//     animeHTML.innerHTML = anime;
-//     console.log(animes["data"])
-// }
-
-// renderAnime();
 async function renderAnime() {
     const animeHTML = document.querySelector(".anime__container");
     const animes = await getData()
-    const anime = Object.values(animes).map((anime) => {
+    const anime = animes["data"].map((anime) => {
         return `<div class="anime__info--container">
         <figure class="anime__img--wrapper">
         <img src="${anime.images.jpg.large_image_url}" alt="" class="anime__img">
@@ -34,14 +12,13 @@ async function renderAnime() {
         <div class= "anime__organizer">
         <h3 class="anime__title">${anime.title} ${animeYear(anime.year)}<br> <a href="${anime.url}" class="anime__link" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a></h3>
         <div class="anime__rating">
-        ${animeRating(anime.score)}
+        ${animeRating(anime.score)}${ratingremainder(anime.score)}
         </div>
         <p class="anime__synopsis">${anime.synopsis}</p>
         </div>
         </div>`
     }).join('')
     animeHTML.innerHTML = anime;
-    console.log(anime)
 }
 
 renderAnime();
@@ -56,7 +33,8 @@ function animeYear (year){
 
 function animeRating (rating){
     let ratingHTML = ""
-    for (let i = 0; i < Math.floor(rating); ++i){
+    for (let i = 0; i < rating; ++i)
+    if (i < Math.floor(rating)){
         ratingHTML += '<i class="fa-solid fa-star"></i>'
     }
     if (!Number.isInteger(rating)){
@@ -64,12 +42,19 @@ function animeRating (rating){
     }
     return ratingHTML
 }
-
+function ratingremainder (rating){
+    let ratingHTML10 = ""
+    for (let i = 1; i < rating; ++i)
+    if (i < 10-rating) {
+        ratingHTML10 += '<i class="fa-regular fa-star"></i>'
+    }
+    return ratingHTML10
+}
 
 async function getData() {
-    const fetchAnime = await fetch(`https://api.jikan.moe/v4/anime/1`);
+    const fetchAnime = await fetch(`https://api.jikan.moe/v4/anime`);
     const animeData = await fetchAnime.json();
-    console.log(animeData)
     return animeData
 }
 
+// https://api.jikan.moe/v4/anime
