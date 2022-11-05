970fe4a721b820aa685e7301bc2a97810fa34006
David <davidmollera@hotmail.com>
2022-11-04 20:19:18 -0500

    converted the object that getData returns into an array, then mapped it using the anime__info--container template and dynamic values from the api

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-async function getData(){
-    const fetchAnime = await fetch('https://api.jikan.moe/v4/anime')
-    const animeData = await fetchAnime.json()
-    console.log(animeData)
-    animeData.map(anime = {
-        
-    })
+
+async function getData() {
+    const fetchAnime = await fetch("https://api.jikan.moe/v4/anime");
+    const animeData = await fetchAnime.json();
+    return animeData
 }
 
-getData()
+
+async function renderAnime() {
+    const animeHTML = document.querySelector(".anime__container");
+    const animes = await getData()
+    const anime = animes["data"].map((anime) => {
+       return `<div class="anime__info--container">
+            <figure class="anime__img--wrapper">
+              <img src="${anime.images.jpg.large_image_url}" alt="" class="anime__img">
+            </figure>
+            <h3 class="anime__title">${anime.title}</h3>
+            <div class="anime__rating">
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-regular fa-star-half-stroke"></i>
+            </div>
+            <p class="anime__synopsis">${anime.synopsis}</p>
+            <a href="${anime.url}" class="anime__link" target="_blank">More info about ${anime.title}</a>
+          </div>`
+      }).join('')
+        animeHTML.innerHTML = anime;
+    console.log(animes["data"])
+}
+
+renderAnime();
+
+
+
+
