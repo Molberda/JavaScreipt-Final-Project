e1c5a219b95bc0ce8ef1ae387c3a85c76e646e44
David <davidmollera@hotmail.com>
2022-11-06 22:04:07 -0500

    changed the api link in order to display more than one result

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const searchRes = document.querySelector('.anime__results')
 const animeHTML = document.querySelector(".anime__container");
 
+// FUNCIONAAA
+// async function renderAnime(title) {
+//     const animes = await getData(title)
+//     const anime = animes.map((anime) => {
+//         return `<div class="anime__info--container">
+//         <figure class="anime__img--wrapper">
+//         <img src="${anime.images.jpg.large_image_url}" alt="" class="anime__img">
+//         </figure>
+//         <div class= "anime__organizer">
+//         <h3 class="anime__title">${anime.title} ${animeYear(anime.year)}<br> <a href="${anime.url}" class="anime__link" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a></h3>
+//         <div class="anime__rating">
+//         ${animeRating(anime.score)}${ratingremainder(anime.score)}
+//         </div>
+//         <p class="anime__synopsis"> <span class="synopsis__title">Synopsis:</span> <br> <br>${anime.synopsis}</p>
+//         </div>
+//         </div>`
+//     }).join('')
+//     setTimeout(() => {
+//         searchRes.innerHTML = `<h2 class="anime__results">Search Results For: <br> <br> <span class="text__orange">${title.replace(/(?<= )[^\s]|^./g, a => a.toUpperCase())}</span></h2>`
+//         animeHTML.innerHTML = anime;
+//     },1000)
+// }
 
 async function renderAnime(title) {
+    searchRes.innerHTML = `<h2 class="anime__results">Search Results For: <br> <br> <span class="text__orange">${title.replace(/(?<= )[^\s]|^./g, a => a.toUpperCase())}</span></h2>`
     const animes = await getData(title)
-    const anime = animes.map((anime) => {
+    console.log(animes["data"])
+    const anime = animes["data"].map((anime) => {
         return `<div class="anime__info--container">
         <figure class="anime__img--wrapper">
         <img src="${anime.images.jpg.large_image_url}" alt="" class="anime__img">
@@ -18,10 +42,7 @@ async function renderAnime(title) {
         </div>
         </div>`
     }).join('')
-    setTimeout(() => {
-        searchRes.innerHTML = `<h2 class="anime__results">Search Results For: <br> <br> <span class="text__orange">${title.replace(/(?<= )[^\s]|^./g, a => a.toUpperCase())}</span></h2>`
-        animeHTML.innerHTML = anime;
-    },1000)
+    animeHTML.innerHTML = anime;
 }
 
 
@@ -52,17 +73,22 @@ function ratingremainder (rating){
     }
     return ratingHTML10
 }
-
+// FUNCIONAAA
+// async function getData(title) {
+//     const fetchAnime = await fetch(`https://api.jikan.moe/v4/anime`);
+//     const animeData = await fetchAnime.json();
+//     const animeArr = Object.values(animeData['data'])
+//     const films = animeArr.filter(animeTitle => {
+//         if (animeTitle.title.toLowerCase() == `${title.toLowerCase()}`){
+//             return true
+//         }
+//     })
+//     return films
+// }
 async function getData(title) {
-    const fetchAnime = await fetch(`https://api.jikan.moe/v4/anime`);
+    const fetchAnime = await fetch(`https://api.jikan.moe/v4/anime?q=${title}`);
     const animeData = await fetchAnime.json();
-    const animeArr = Object.values(animeData['data'])
-    const films = animeArr.filter(animeTitle => {
-        if (animeTitle.title.toLowerCase() == `${title.toLowerCase()}`){
-            return true
-        }
-    })
-    return films
+    return animeData
 }
 
 // https://api.jikan.moe/v4/anime
